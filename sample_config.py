aad87dff2f9dd72f59ca985fe97c8a51bb192dd9
sanith2005 <77660641+sanith2005@users.noreply.github.com>
2021-06-15 20:24:47 -0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,8 +6,10 @@ if HEROKU:
 
     Bot_token = environ["Bot_token"]
     ARQ_API_KEY = environ["ARQ_API_KEY"]
+    UPDATES_CHANNEL = environ["UPDATES_CHANNEL"]
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
     Bot_token = "Insert Bot_Token Here"
     ARQ_API_KEY = "Get this from @ARQRobot"
+    UPDATES_CHANNEL = "channel"
